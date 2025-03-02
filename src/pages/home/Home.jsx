7091daeedfc6814674d1296efe48e877148ff4cf
kasra <kasramajdy81@gmail.com>
2025-03-02 12:05:27 +0330

    adding like and unsave and get comment

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,8 @@
 import Navbar from "../../components/Layout/Navbar";
 import Posts from "../../components/Posts/Posts";
-
 export default function Home() {
   return (
-    <main>
+    <main className={"flex items-start"}>
         <Navbar/>
         <Posts />
     </main>
