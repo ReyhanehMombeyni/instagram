a34cb804860419bbd9983754a8adeed17cb78616
hamid <hamid.a7832@gmail.com>
2025-03-02 16:40:55 +0330

    add img and ui design

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Login from "./pages/auth/Login"
 import Home from "./pages/home/Home"
 import SearchBar from "./components/search/SearchBar"
 import Layout from "./components/Layout/Layout"
-
+import Profile from "./components/profile/Profile"
 const router = createBrowserRouter([
     {
       path: "/",
@@ -26,6 +26,10 @@ const router = createBrowserRouter([
         {
           path: "/search",
           element: <SearchBar />
+        },
+        {
+          path: "/profile",
+          element: <Profile/>
         }
       ]
     }
